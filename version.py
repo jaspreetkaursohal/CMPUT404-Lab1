1ea3c5d8170528ff78030ef023d3eaafdeddc001
jaspreetkaursohal <jsohal@ualberta.ca>
2023-01-09 17:27:35 -0700

    updated python script

--- version.py
+++ version.py
@@ -1,3 +1,4 @@
 import requests
 
-print(requests.get('http://www.google.com/'))
+response = requests.get('https://raw.githubusercontent.com/jaspreetkaursohal/CMPUT404-Lab1/main/version.py')
+print(response._content)
